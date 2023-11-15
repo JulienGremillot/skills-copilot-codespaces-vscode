278fe4c7d16fa0bd94f691fc45bd0726feade39c
Julien Gremillot <julien@gremillot.net>
2023-11-15 10:10:38 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'modules/skills/views/member.html',
+    controller: 'SkillsMemberController',
+    controllerAs: 'vm',
+    scope: {
+      member: '=',
+      showSkills: '='
+    },
+    bindToController: true
+  };
+}
